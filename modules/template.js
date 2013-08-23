782923a130391d7f9114e9e61d1b6a6a1c5cd1a9
Will Ayers <willayers.net@gmail.com>
2013-08-23 16:34:52 -0600

    Boomsauce we understand it now:

--- modules/template.js
+++ modules/template.js
@@ -1,26 +1,26 @@
-var template_cache = {};
+var template_cache = {},
+    Template;
 
-function tpl(template, data) {
+Template = function () {
     var content = template_cache[template];
 
     if (!content) {
         content = "var s='';s+=\'" +
-            template.replace(/[\r\t\n]/g, " ")
-            .split("'").join("\\'")
-            .split("\t").join("'")
-            .replace(/\{\{#([\w]*)\}\}(.*)\{\{\/(\1)\}\}/ig, function (match, $1, $2) {
-                return "\';var i=0,l=data." + $1 +
-                    ".length,d=data." + $1 +
-                    ";for(;i<l;i++){s+=\'" +
-                    $2.replace(/\{\{(\.|this)\}\}/g, "'+d[i]+'")
-                        .replace(/\{\{([\w]*)\}\}/g, "'+d[i].$1+'") +
-                    "\'}s+=\'";
-            })
-            .replace(/\{\{(.+?)\}\}/g, "'+data.$1+'") +
-            "';return s;";
+            template
+                .replace(/[\r\t\n]/g, " ")
+                .split("'").join("\\'")
+                .split("\t").join("'")
+                .replace(/\{\{#([\w]*)\}\}(.*)\{\{\/(\1)\}\}/ig, function (match, $1, $2) {
+                    return "\';var i=0,l=data." + $1 +".length,d=data." + $1 + ";for(;i<l;i++){s+=\'" +
+                        $2.replace(/\{\{(\.|this)\}\}/g, "'+d[i]+'")
+                            .replace(/\{\{([\w]*)\}\}/g, "'+d[i].$1+'") +
+                        "\'}s+=\'";
+                })
+                .replace(/\{\{(.+?)\}\}/g, "'+data.$1+'") +
+                "';return s;";
 
         template_cache[template] = content;
     }
 
     return data ? new Function("data", content)(data) : new Function("data", content);
-}
+};
