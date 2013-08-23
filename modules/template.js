b2117893f8fbbb9eb84653721d23c1d352e95f06
Will Ayers <willayers.net@gmail.com>
2013-08-23 17:01:27 -0600

    Because I can

--- modules/template.js
+++ modules/template.js
@@ -1,26 +1,30 @@
 var template_cache = {},
     Template;
 
-Template = function () {
+Template = function (template, data) {
     var content = template_cache[template];
 
-    if (!content) {
-        content = "var s='';s+=\'" +
-            template
-                .replace(/[\r\t\n]/g, " ")
-                .split("'").join("\\'")
-                .split("\t").join("'")
-                .replace(/\{\{#([\w]*)\}\}(.*)\{\{\/(\1)\}\}/ig, function (match, $1, $2) {
-                    return "\';var i=0,l=data." + $1 +".length,d=data." + $1 + ";for(;i<l;i++){s+=\'" +
-                        $2.replace(/\{\{(\.|this)\}\}/g, "'+d[i]+'")
-                            .replace(/\{\{([\w]*)\}\}/g, "'+d[i].$1+'") +
-                        "\'}s+=\'";
-                })
-                .replace(/\{\{(.+?)\}\}/g, "'+data.$1+'") +
-                "';return s;";
+    if (! content) {
+        content = TemplateCreate(template);
 
         template_cache[template] = content;
     }
 
     return data ? new Function("data", content)(data) : new Function("data", content);
+};
+
+TemplateCreate = function (template) {
+    return "var s='';s+=\'" +
+        template
+            .replace(/[\r\t\n]/g, " ")
+            .split("'").join("\\'")
+            .split("\t").join("'")
+            .replace(/\{\{#([\w]*)\}\}(.*)\{\{\/(\1)\}\}/ig, function (match, $1, $2) {
+                return "\';var i=0,l=data." + $1 +".length,d=data." + $1 + ";for(;i<l;i++){s+=\'" +
+                    $2.replace(/\{\{(\.|this)\}\}/g, "'+d[i]+'")
+                        .replace(/\{\{([\w]*)\}\}/g, "'+d[i].$1+'") +
+                    "\'}s+=\'";
+            })
+            .replace(/\{\{(.+?)\}\}/g, "'+data.$1+'") +
+            "';return s;";
 };
