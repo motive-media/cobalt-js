a867a187add54488c342281e31c7cdb81bb690ca
Will Ayers <willayers.net@gmail.com>
2013-09-10 11:17:06 -0600

    Added ngmin to grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,12 +16,28 @@ module.exports = function (grunt) {
                     }
                 ]
             }
+        },
+
+        ngmin: {
+            base_files: {
+                expand: true,
+                cwd: 'tmp',
+                src: ['*.js'],
+                dest: 'build'
+            },
+            directives: {
+                expand: true,
+                cwd: 'tmp',
+                src: ['directives/**/*.js'],
+                dest: 'build'
+            }
         }
     });
 
     // Load tasks
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-ngmin');
 
     // Register tasks
     //grunt.registerTask('default', ['']);
