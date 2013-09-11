4886e56921fd249d2b24fb3825251b10c11a4901
Will Ayers <willayers.net@gmail.com>
2013-09-11 17:34:36 -0600

    Fixed position calculation to take into account margin + padding. Made demos better.

--- angularjs/directives/cbTooltip.js
+++ angularjs/directives/cbTooltip.js
@@ -55,6 +55,25 @@ angular.module('cbTooltip', []).directive('cbTooltip', function () {
                             top = 0,
                             left = 0;
 
+                        var ePadding = {
+                                top: element.css('padding-top'),
+                                right: element.css('padding-right'),
+                                bottom: element.css('padding-bottom'),
+                                left: element.css('padding-left')
+                            },
+                            eMargin = {
+                                top: element.css('margin-top'),
+                                right: element.css('margin-right'),
+                                bottom: element.css('margin-bottom'),
+                                left: element.css('margin-left')
+                            };;
+
+                        eWidth += parseInt(ePadding.left.replace('px', ''), 10) + parseInt(ePadding.right.replace('px', ''), 10);
+                        eHeight += parseInt(ePadding.top.replace('px', ''), 10) + parseInt(ePadding.bottom.replace('px', ''), 10);
+
+                        eWidth += parseInt(eMargin.left.replace('px', ''), 10) + parseInt(eMargin.right.replace('px', ''), 10);
+                        eHeight += parseInt(eMargin.top.replace('px', ''), 10) + parseInt(eMargin.bottom.replace('px', ''), 10);
+
                         if ('top' === tooltip.position) {
                             top = os.top - tHeight - tooltip.space;
                         } else if ('bottom' === tooltip.position) {
